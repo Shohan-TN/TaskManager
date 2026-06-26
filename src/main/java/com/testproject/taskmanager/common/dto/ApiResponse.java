e09f1e9a0a776a77bc8432177decd9792671ff31
S. M. Shohanur Islam <smshohan639@gmail.com>
2026-06-26 18:45:21 +0600

    Bug fix in  api response

--- src/main/java/com/testproject/taskmanager/common/dto/ApiResponse.java
+++ src/main/java/com/testproject/taskmanager/common/dto/ApiResponse.java
@@ -10,21 +10,22 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse <T> {
-    private int status;
+    private String status;
+    private int code;
     private String message;
     private T data;
     private ErrorResponse error;
     private MetaData meta;
 
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(200, message, data, null, new  MetaData(LocalDateTime.now()));
+        return new ApiResponse<>("success",200, message, data, null, new  MetaData(LocalDateTime.now()));
     }
 
     public static <T> ApiResponse<T> success(String message) {
-        return new ApiResponse<>(200, message, null, null, new MetaData(LocalDateTime.now()));
+        return new ApiResponse<>("success",200, message, null, null, new MetaData(LocalDateTime.now()));
     }
 
-    public static <T> ApiResponse<T> error(int status, String message, ErrorResponse error) {
-        return new ApiResponse<>(status, message, null, error, new MetaData(LocalDateTime.now()));
+    public static <T> ApiResponse<T> error(int code, String message, ErrorResponse error) {
+        return new ApiResponse<>("error",code, message, null, error, new MetaData(LocalDateTime.now()));
     }
 }
